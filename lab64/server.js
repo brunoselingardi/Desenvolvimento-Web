12d0d0079720c109f3bb20c9c5cc04fcba1aea9b
brunoselingardi <brunoselingardi@gmail.com>
2024-09-25 20:24:10 -0300

    adção de 'post' e 404 no lab64

--- lab64/server.js
+++ lab64/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const path = require('path');
-
-
-
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded( {extended: true }));
+
 // Configura o Express para servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,11 +19,18 @@ app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+app.get('/formulario', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'formulario.html'));
 });
 
+app.use((req,res) => {
+    res.status(404).send('ERRO 404 - página não encontrada)');
+});
 
+app.post('/formulario', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 // Inicia o servidor
 app.listen(port, () => {
