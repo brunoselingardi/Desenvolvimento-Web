c4ec59bf4dec778ab44510cb89712b8b788c9358
brunoselingardi <brunoselingardi@gmail.com>
2024-09-25 15:10:19 -0300

    lab64 para terminar

---
+++ lab64/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+
+
+const app = express();
+const port = 3000;
+// Configura o Express para servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Rota para a página "Sobre"
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'upload.html'));
+});
+
+
+
+// Inicia o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
