3d8b04337f1b9450c7e8654b65e6fb5a2ea11211
brunoselingardi <brunoselingardi@gmail.com>
2024-10-01 19:29:19 -0300

    mudança no server.js e post (com postman)

--- lab64/server.js
+++ lab64/server.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
+const fictionalUser = require("./usuario");
+const bodyParser = require('body-parser');
 
-app.use(express.json());
-app.use(express.urlencoded( {extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/api', (req, res) => {
+    console.log(req.body);
+    const newUsuario = {
+        id: req.body.id,
+        nome: req.body.nome
+    }
+    fictionalUser.push(newUsuario);
+    res.json({ message: 'TESTE' });
+});
 
 // Configura o Express para servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
@@ -27,11 +39,10 @@ app.use((req,res) => {
     res.status(404).send('ERRO 404 - página não encontrada)');
 });
 
-app.post('/formulario', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.get('/formu', (req, res) => {
+    res.send(`<h1>usuario ficticio</h1>
+        <h4>${fictionalUser[0].nome}</h4>`)
 });
-
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
---
+++ lab64/usuario.js
@@ -0,0 +1,12 @@
+const fictionalUser = [
+    {
+        id: 1,
+        nome: "Roberto"
+    },
+    {
+        id: 2,
+        nome: "Willian"
+    }
+];
+
+module.exports = fictionalUser;
